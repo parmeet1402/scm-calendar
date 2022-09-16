53ba48bebc4c9b513abc53423a49e3997c850974
Parmeet Singh <parmeet.asi@gmail.com>
2022-09-16 14:21:09 +0530

    Add ability to accept slot events

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,6 +1,6 @@
-import { Component, Prop, h } from '@stencil/core';
-5;
-import { format } from '../../utils/utils';
+import { Component, Prop, h, Element } from '@stencil/core';
+
+// import { format } from '../../utils/utils';
 // ----
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -27,22 +27,54 @@ import listPlugin from '@fullcalendar/list';
 
 // calendar.render();
 
+function hasSlot(element: HTMLElement, slotName: string): boolean {
+  return element.querySelector<HTMLSlotElement>(`[slot="${slotName}"]`) !== null;
+}
+
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
   shadow: true,
 })
 export class MyComponent {
-  componentDidRender() {
-    const calendarEl = document.querySelector('#calendar');
-    console.log({ calendarEl });
-    const calendar = new Calendar(calendarEl as HTMLElement, {
-      plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+  container!: HTMLDivElement;
+  calendarContainerEl!: HTMLDivElement;
+
+  // @Element() private element: HTMLElement;
+
+  componentDidLoad() {
+    console.log({ calendarContainerEl: this.calendarContainerEl }); // outputs HTMLElement <my-component ...
+
+    const calendar = new Calendar(this.calendarContainerEl as HTMLDivElement, {
       initialView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,listWeek',
+      initialDate: '2022-08-12',
+      plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+
+      eventColor: '#f00',
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2022-08-01',
+          id: '123',
+          // RElement: <slot name="event-123" />,
+        },
+      ],
+      eventContent: args => {
+        // console.log({ args });
+        // console.log({ calend: this.container, parent: this.container.parentNode });
+        // const parent = this.container.parentNode;
+        // console.log({ parent });
+        // const slotElements = parent.querySelector('slot');
+        // hasSlot and then look 
+        // console.log({ slotElements });
+        // console.log({ slotElements });
+
+        // querySelectorAll select all slots inside a wrapper
+        // from that we could
+
+        return {
+          html: `<slot name="event"/>`,
+        };
       },
     });
     calendar.render();
@@ -50,13 +82,16 @@ export class MyComponent {
 
   render() {
     return (
-      <div
-        id="calendar"
-        style={{
-          height: '500px',
-          width: '500px',
-        }}
-      />
+      <div ref={el => (this.container = el as HTMLDivElement)}>
+        <div
+          ref={el => (this.calendarContainerEl = el as HTMLDivElement)}
+          // id="calendar"
+          style={{
+            height: '1200px',
+            width: '1200px',
+          }}
+        />
+      </div>
     );
   }
 }
